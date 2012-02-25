40f3d74c9eeb4425972ea1deac93b5a1f414e0c3
Eugeny Fomin <github.com@jeka.ru>
2012-02-25 21:42:48 +0400

    fix

--- app/Application/Vespolina/StoreBundle/Document/Store.php
+++ app/Application/Vespolina/StoreBundle/Document/Store.php
@@ -2,23 +2,5 @@
 namespace Application\Vespolina\StoreBundle\Document;
 
 
-class Store extends \Vespolina\StoreBundle\Document\BaseStore{
-
-
-
-    /**
-     * @var MongoId $id
-     */
-    protected $id;
-
-
-    /**
-     * Get id
-     *
-     * @return id $id
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
+class Store {
 }
